17c16377169a718075a70e249280af52319c2836
yash2198 <yashchowdhary98@gmail.com>
2017-12-20 14:09:45 +0800

    created all pouchDBs using test.js

--- test.js
+++ test.js
@@ -1,32 +1,43 @@
 var PouchDB = require('pouchdb');
 PouchDB.plugin(require('pouchdb-load'));
 const fs = require('fs');
+var http = require('http');
+var https = require('https');
 const couchbackup = require('@cloudant/couchbackup');
 
-var remote_db = new PouchDB('http://localhost:5984/test_db_3');
+var body='';
+var request = https.get("https://manareasedinguallyarthei:546b7799281fc1eb8836d7f509d7cbaac24c375c@vaultdragon.cloudant.com/vaultdragon/_all_docs?include_docs=true",
+function(res){
+	res.on('data', function(chunk){
+		body+=chunk;
+	});
+	res.on('end',function(){
+		body = body.trim();
+		var object = JSON.parse(body);
+		for(let i=0;i<35;i++){
+			if(object.rows[i].id.substring(0,1) != '_'){        //filters out ones that don't have nested databases
+				if(object.rows[i].doc.hasOwnProperty('cms')){   //filters out the ones that have cms & dragonfly databases
+					var cmsDbUrl = 'http://localhost:5984/' + object.rows[i].doc.cms.cloudantDatabase.toString();
+					var dragonflyDbUrl = 'http://localhost:5984/' + object.rows[i].doc.dragonfly.cloudantDatabase.toString();
+					console.log(cmsDbUrl);
+					console.log(dragonflyDbUrl);
+					
+					var remote_db_cms = new PouchDB(cmsDbUrl);
+					var remote_db_dragonfly = new PouchDB(dragonflyDbUrl);
 
-remote_db.info().then(function (info) {
-    console.log(info);
-  })
-
-let filename = 'test_backup.txt';
-
-// couchbackup.restore(
-// fs.createReadStream(filename),
-// 'http://localhost:5984/test_db',
-// {parallelism: 5},
-// function(err, data) {
-//     if (err) {
-//     console.error("Failed! " + err);
-//     } else {
-//     console.error("Success! " + data);
-//     }
-// });
-// remote_db.load(filename).then(function () {
-//   // done loading!
-//   console.log("Done")
-// }).catch(function (err) {
-//   // HTTP error or something like that
-//   console.log("Nope")
-// });  
-// //couchbackup.restore('test_backup.txt');
+					remote_db_cms.info().then(function (info) {
+						console.log('db added');
+					})
+					remote_db_dragonfly.info().then(function (info) {
+						console.log('db added');
+					})
+				}   else{
+						var remote_db = new PouchDB('http://localhost:5984/'+object.rows[i].doc.cloudantDatabase);
+						remote_db.info().then(function (info) {
+							console.log('db added');
+						})
+				}
+			}
+		}
+	});
+})
