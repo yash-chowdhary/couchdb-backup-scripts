d3bbb5023c5198aaae57ce0bb76f20ca296a0f16
yash2198 <yashchowdhary98@gmail.com>
2017-12-25 22:11:00 +0800

    added all params -> run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 node test.js $1 $2 $3
 
-node extract_db.js $1 $2 $3
+node extract_db.js $1 $2 $3 $4 $5 $6
 
-node replace.js $1 $2 $3
+node replace.js $1 $2 $3 $4 $5 $6
 
-node restore.js $1 $2 $3
+node restore.js $1 $2 $3 $4 $5 $6
 
-node request_helper.js $1 $2 $3
+node request_helper.js $1 $2 $3 $4 $5 $6
