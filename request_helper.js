f5e98a9b7931ae3e6c05da836126f9afdc5acc19
yash2198 <yashchowdhary98@gmail.com>
2017-12-25 21:48:45 +0800

    PM2 modification -> request (test & upload)

--- request_helper.js
+++ request_helper.js
@@ -2,56 +2,16 @@ var cmd = require('node-cmd');
 var http = require('http');
 var https = require('https');
 
-var param_username = process.argv[2].toString();
-var param_password = process.argv[3].toString();
-var param_dbname = process.argv[4].toString();
+var main_param_username = process.argv[2].toString();
+var main_param_password = process.argv[3].toString();
+var main_param_dbname = process.argv[4].toString();
+var param_username = process.argv[5].toString();
+var param_password = process.argv[6].toString();
+var param_dbname = process.argv[7].toString();
 
-var body='';
-var request = https.get("https://"+param_username+":"+param_password+"@vaultdragon.cloudant.com/"+param_dbname+"/_all_docs?include_docs=true",
-function(res){
-    res.on('data', function(chunk){
-        body+=chunk;           
-    });
-    res.on('end',function(){
-        body = body.trim();
-        // console.log('body: \n'+body);
-        var object = JSON.parse(body);
-        for(let i=0;i<35;i++){
-            if(object.rows[i].id.substring(0,1) != '_'){        //filters out ones that don't have nested databases
-                if(object.rows[i].doc.hasOwnProperty('cms')){   //filters out the ones that have cms & dragonfly databases
-                    var cms_username = object.rows[i].doc.cms.cloudantUsername;
-                    var cms_password = object.rows[i].doc.cms.cloudantPassword;
-                    var cms_database = object.rows[i].doc.cms.cloudantDatabase;
-                    var cms_cmdCommand = 'request_caller.sh '+cms_username+' '+cms_password+' '+cms_database;
-                    cmd.get(cms_cmdCommand,function(err,data,stderr){
-                        console.log('run complete');
-                        console.log(stderr);
-                        console.log(err);
-                    });
-
-                    if(i!=3){
-                        var dragonfly_username = object.rows[i].doc.dragonfly.cloudantUsername;
-                        var dragonfly_password = object.rows[i].doc.dragonfly.cloudantPassword;
-                        var dragonfly_database = object.rows[i].doc.dragonfly.cloudantDatabase;
-                        var dragonfly_cmdCommand = 'request_caller.sh '+dragonfly_username+' '+dragonfly_password+' '+dragonfly_database;
-                        cmd.get(dragonfly_cmdCommand,function(err,data,stderr){
-                            console.log('run complete');
-                            console.log(stderr);
-                            console.log(err);
-                        });
-                    }
-                }   else{
-                    var username = object.rows[i].doc.cloudantUsername;
-                    var password = object.rows[i].doc.cloudantPassword;
-                    var database = object.rows[i].doc.cloudantDatabase;
-                    var cmdCommand = 'request_caller.sh '+username+' '+password+' '+database;
-                    cmd.get(cmdCommand,function(err,data,stderr){
-                        console.log('run complete');
-                        console.log(stderr);
-                        console.log(err);
-                    });
-                }
-            }
-        }
-    });
-})
+var cmdCommand = 'request_caller.sh '+param_username+' '+param_password+' '+param_dbname;
+cmd.get(cmdCommand,function(err,data,stderr){
+    console.log('run complete');
+    console.log(stderr);
+    console.log(err);
+});
