e11ef7a40bd51c21b9f423265794070735b85b69
yash2198 <yashchowdhary98@gmail.com>
2017-12-25 21:45:35 +0800

    PM2 modification -> restore.js + testscript.sh

--- restore.js
+++ restore.js
@@ -2,53 +2,17 @@ var cmd = require('node-cmd');
 var http = require('http');
 var https = require('https');
 
-var param_username = process.argv[2].toString();
-var param_password = process.argv[3].toString();
-var param_dbname = process.argv[4].toString();
+var main_param_username = process.argv[2].toString();
+var main_param_password = process.argv[3].toString();
+var main_param_dbname = process.argv[4].toString();
+var param_username = process.argv[5].toString();
+var param_password = process.argv[6].toString();
+var param_dbname = process.argv[7].toString();
 
-var body='';
-var request = https.get("https://"+param_username+":"+param_password+"@vaultdragon.cloudant.com/"+param_dbname+"/_all_docs?include_docs=true",
-function(res){
-    res.on('data', function(chunk){
-        body+=chunk;
-    });
-    res.on('end',function(){
-        body = body.trim();
-        // console.log('body: \n'+body);
-        var object = JSON.parse(body);
-        for(let i=0;i<35;i++){
-            if(object.rows[i].id.substring(0,1) != '_'){        //filters out ones that don't have nested databases
-                if(object.rows[i].doc.hasOwnProperty('cms')){   //filters out the ones that have cms & dragonfly databases
-                    var cmsBackupFileName = 'decrypted_'+object.rows[i].doc.cms.cloudantDatabase.toString()+'.txt';
-                    var dragonflyBackupFileName = 'decrypted_'+object.rows[i].doc.dragonfly.cloudantDatabase.toString()+'.txt';
-                    
-                    var cms_cmdCommand_restore = 'testscript.sh '+cmsBackupFileName+' '+object.rows[i].doc.cms.cloudantDatabase.toString();
-                    
-                    var dragonfly_cmdCommand_restore = 'testscript.sh '+dragonflyBackupFileName+' '+object.rows[i].doc.dragonfly.cloudantDatabase.toString();
+var backupFileName = 'decrypted_'+param_dbname+'.txt';
+var cmdCommand_restore = 'testscript.sh '+backupFileName+' '+param_dbname;
 
-                    console.log(dragonfly_cmdCommand_restore);
-                    //call couchbackup                    
-                    cmd.get(cms_cmdCommand_restore,function(){
-                        console.log('(cms) restore complete: '+object.rows[i].doc.cms.cloudantDatabase);
-                    })
-
-                    if(i!=3){
-                        cmd.get(dragonfly_cmdCommand_restore,function(){
-                            console.log('(dragonfly) restore complete: '+object.rows[i].doc.dragonfly.cloudantDatabase);
-                        })   
-                    }             
-                }   else{
-                    
-                    var backupFileName = 'decrypted_'+object.rows[i].doc.cloudantDatabase.toString()+'.txt';
-                    var cmdCommand_restore = 'testscript.sh '+backupFileName+' '+object.rows[i].doc.cloudantDatabase.toString();
-                    
-                    //call couchbackup
-                    cmd.get(cmdCommand_restore,function(err,data,stderr){
-                        console.log('restore complete.');
-                    })
-
-                }
-            }
-        }
-    });
-})
+//call couchbackup
+cmd.get(cmdCommand_restore,function(err,data,stderr){
+    console.log('restore complete.');
+});
