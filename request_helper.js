f07cc2e71efb4a941480b2fb317c4ecf1912687c
yash2198 <yashchowdhary98@gmail.com>
2017-12-25 22:14:00 +0800

    corrected param changes -> request_helper

--- request_helper.js
+++ request_helper.js
@@ -2,10 +2,10 @@ var cmd = require('node-cmd');
 var http = require('http');
 var https = require('https');
 
-var main_param_username = process.argv[2].toString();
+var main_param_username = process.argv[2].toString();   //vd account
 var main_param_password = process.argv[3].toString();
 var main_param_dbname = process.argv[4].toString();
-var param_username = process.argv[5].toString();
+var param_username = process.argv[5].toString();        //nested DBs
 var param_password = process.argv[6].toString();
 var param_dbname = process.argv[7].toString();
 
